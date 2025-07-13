43c4868da060bcee41d95ec23bff879cffc60a9c
ceferhesenli <ceferhesenli27@gmail.com>
2025-07-13 06:06:04 -0700

    Add files via upload
    to do list davam

--- front end developer dərsi/javascript/script.js
+++ front end developer dərsi/javascript/script.js
@@ -2,21 +2,42 @@ const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+let tasks = Json.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  taskList.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "🗑️";
+    delBtn.onclick = () => {
+      tasks.splice(index, 1);
+      saveTasks();
+    };
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+  });
+}
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
+
 addBtn.addEventListener("click", () => {
   const taskText = taskInput.value.trim();
-
   if (taskText !== "") {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      ${taskText}
-      <button class="deleteBtn">Sil</button>
-    `;
-    taskList.appendChild(li);
-
-    li.querySelector(".deleteBtn").addEventListener("click", () => {
-      li.remove();
-    });
-
+    tasks.push(taskText);
+    saveTasks();
     taskInput.value = "";
   }
 });
+
+taskInput.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    addBtn.click();
+  }
+});
+
+// İlk açılışda siyahını göstər
+renderTasks();
